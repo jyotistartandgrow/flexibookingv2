8d977f96a2633e525e6be051382f1e1b70aba9a9
jyotistartandgrow <jyotiprava@startandgrow.in>
2026-02-10 20:20:24 +0530

    thank you

--- src/pages/Thankyou.jsx
+++ src/pages/Thankyou.jsx
@@ -17,6 +17,7 @@ export default function Thankyou() {
   const dispatch = useDispatch();
   const componentRef = useRef();
   const bookingkey = useSelector((state) => state.step3.bookingkey);
+  const loading = useSelector((state) => state.step1.loading);
   const [bookingData, setBookingData] = useState(null);
   const [email, setEmail] = useState("");
 
@@ -96,176 +97,181 @@ export default function Thankyou() {
     bookingdetail();
   }, []);
   return (
-    <div className="fx-booking fx-container">
-      <div className="fx-confirmation-wrapper">
-        <img
-          src={logo}
-          className="fx-thanku-logo"
-          onClick={() => (window.location.href = "/")}
-        />
-        <div className="fx-confirm-left">
-          <div className="fx-voucher-icon">
-            <i className="pi pi-file-pdf"></i>
-            <span>Voucher</span>
-          </div>
-
-          <h1>
-            Hey {bookingData?.customer_billing?.billing_first_name}{" "}
-            {bookingData?.customer_billing?.billing_last_name}!
-          </h1>
-          <h2>Thanks for Your Order.</h2>
-
-          <p className="fx-info-text">
-            You will receive an email with the ticket, show it when you arrive
-            at the Spa. If you book with a voucher, you must present your QC
-            Pass, Esselunga voucher or company welfare vouchers in the purchase
-            format at the reception in order to take advantage of the services
-            included.
-          </p>
-
-          <div className="fx-email-box">
-            <input
-              type="text"
-              placeholder="Send via mail"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <button className="fx-btn-send" onClick={sendEmail}>
-              Send
-            </button>
-          </div>
-
-          <div className="fx-button-row">
-            <button className="fx-btn-light" onClick={handlePrint}>
-              {" "}
-              <span className="fx-thankubuttontext">Print</span>{" "}
-              <i className="pi pi-print"></i>
-            </button>
-            <button className="fx-btn-light" onClick={downloadAsPDF}>
-              {" "}
-              <span className="fx-thankubuttontext">Download</span>{" "}
-              <i className="pi pi-download"></i>
-            </button>
-          </div>
-        </div>
-        <div className="fx-confirm-right" ref={componentRef}>
-          <div className="fx-confirm-innerrightbox">
-            <h3 className="fx-order-title">
-              Your order is Confirmed. You will receive a confirmation mail in
-              your billing email.
-            </h3>
-
-            <div className="fx-order-row">
-              <div>
-                Service Date:
-                <br />{" "}
-                <span>
-                  {moment(bookingData?.service_date).format("MMMM Do, YYYY")}
-                </span>
-              </div>
-              <div>
-                Payment via: <br />
-                <span>Card</span>
-              </div>
+    <>
+      <div className={`fx-fullscreen-loader ${loading ? "show" : "hide"}`}>
+        <div className="fx-seg-loader"></div>
+      </div>
+      <div className="fx-booking fx-container">
+        <div className="fx-confirmation-wrapper">
+          <img
+            src={logo}
+            className="fx-thanku-logo"
+            onClick={() => (window.location.href = "/")}
+          />
+          <div className="fx-confirm-left">
+            <div className="fx-voucher-icon">
+              <i className="pi pi-file-pdf"></i>
+              <span>Voucher</span>
             </div>
 
-            <div className="fx-order-row">
-              <div>
-                Order Ref:
-                <br />
-                <span>{bookingData?.payment_ref_id}</span>
-              </div>
+            <h1>
+              Hey {bookingData?.customer_billing?.billing_first_name}{" "}
+              {bookingData?.customer_billing?.billing_last_name}!
+            </h1>
+            <h2>Thanks for Your Order.</h2>
+
+            <p className="fx-info-text">
+              You will receive an email with the ticket, show it when you arrive
+              at the Spa. If you book with a voucher, you must present your QC
+              Pass, Esselunga voucher or company welfare vouchers in the
+              purchase format at the reception in order to take advantage of the
+              services included.
+            </p>
+
+            <div className="fx-email-box">
+              <input
+                type="text"
+                placeholder="Send via mail"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <button className="fx-btn-send" onClick={sendEmail}>
+                Send
+              </button>
             </div>
 
-            <table className="fx-order-table">
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>Product</th>
-                  <th>Price</th>
-                  <th>Qty</th>
-                  <th>Total</th>
-                </tr>
-              </thead>
+            <div className="fx-button-row">
+              <button className="fx-btn-light" onClick={handlePrint}>
+                {" "}
+                <span className="fx-thankubuttontext">Print</span>{" "}
+                <i className="pi pi-print"></i>
+              </button>
+              <button className="fx-btn-light" onClick={downloadAsPDF}>
+                {" "}
+                <span className="fx-thankubuttontext">Download</span>{" "}
+                <i className="pi pi-download"></i>
+              </button>
+            </div>
+          </div>
+          <div className="fx-confirm-right" ref={componentRef}>
+            <div className="fx-confirm-innerrightbox">
+              <h3 className="fx-order-title">
+                Your order is Confirmed. You will receive a confirmation mail in
+                your billing email.
+              </h3>
 
-              <tbody>
-                {bookingData?.products?.map((product, pkey) => (
-                  <tr key={"product-" + pkey}>
-                    <td>{product.product_heading}</td>
-                    <td>{product.name}</td>
-                    <td>{decodeHtml(product.price)}</td>
-                    <td>{product.quantity}</td>
-                    <td>{decodeHtml(product.total)}</td>
+              <div className="fx-order-row">
+                <div>
+                  Service Date:
+                  <br />{" "}
+                  <span>
+                    {moment(bookingData?.service_date).format("MMMM Do, YYYY")}
+                  </span>
+                </div>
+                <div>
+                  Payment via: <br />
+                  <span>Card</span>
+                </div>
+              </div>
+
+              <div className="fx-order-row">
+                <div>
+                  Order Ref:
+                  <br />
+                  <span>{bookingData?.payment_ref_id}</span>
+                </div>
+              </div>
+
+              <table className="fx-order-table">
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>Product</th>
+                    <th>Price</th>
+                    <th>Qty</th>
+                    <th>Total</th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
+                </thead>
 
-            <div className="fx-summary">
-              <div>
-                Subtotal{" "}
-                <span>
-                  {decodeHtml(bookingData?.product_details?.subtotal)}
-                </span>
-              </div>
-              <div>
-                Discount<span>{decodeHtml(bookingData?.coupon_discount)}</span>
-              </div>
-              <div>
-                Total
-                <span>{decodeHtml(bookingData?.product_details?.total)}</span>
-              </div>
-            </div>
+                <tbody>
+                  {bookingData?.products?.map((product, pkey) => (
+                    <tr key={"product-" + pkey}>
+                      <td>{product.product_heading}</td>
+                      <td>{product.name}</td>
+                      <td>{decodeHtml(product.price)}</td>
+                      <td>{product.quantity}</td>
+                      <td>{decodeHtml(product.total)}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
 
-            <div className="fx-address-block">
-              <div>
-                <h4>Billing Address</h4>
-                <p>
-                  {bookingData?.customer_billing?.billing_first_name}{" "}
-                  {bookingData?.customer_billing?.billing_last_name} <br />
-                  {bookingData?.customer_billing?.billing_address} <br />
-                  {bookingData?.customer_billing?.billing_city} <br />
-                  {bookingData?.customer_billing?.billing_email}
-                </p>
+              <div className="fx-summary">
+                <div>
+                  Subtotal{" "}
+                  <span>
+                    {decodeHtml(bookingData?.product_details?.subtotal)}
+                  </span>
+                </div>
+                <div>
+                  Discount
+                  <span>{decodeHtml(bookingData?.coupon_discount)}</span>
+                </div>
+                <div>
+                  Total
+                  <span>{decodeHtml(bookingData?.product_details?.total)}</span>
+                </div>
               </div>
 
-              <div>
-                <h4>Shipping Address</h4>
-                <p>
-                  {bookingData?.customer_shipping?.shipping_first_name}{" "}
-                  {bookingData?.customer_shipping?.shipping_last_name} <br />
-                  {bookingData?.customer_shipping?.shipping_address} <br />
-                  {bookingData?.customer_shipping?.shipping_city} <br />
-                  {bookingData?.customer_shipping?.shipping_email}
-                </p>
+              <div className="fx-address-block">
+                <div>
+                  <h4>Billing Address</h4>
+                  <p>
+                    {bookingData?.customer_billing?.billing_first_name}{" "}
+                    {bookingData?.customer_billing?.billing_last_name} <br />
+                    {bookingData?.customer_billing?.billing_address} <br />
+                    {bookingData?.customer_billing?.billing_city} <br />
+                    {bookingData?.customer_billing?.billing_email}
+                  </p>
+                </div>
+
+                <div>
+                  <h4>Shipping Address</h4>
+                  <p>
+                    {bookingData?.customer_shipping?.shipping_first_name}{" "}
+                    {bookingData?.customer_shipping?.shipping_last_name} <br />
+                    {bookingData?.customer_shipping?.shipping_address} <br />
+                    {bookingData?.customer_shipping?.shipping_city} <br />
+                    {bookingData?.customer_shipping?.shipping_email}
+                  </p>
+                </div>
               </div>
-            </div>
 
-            <table className="fx-billing-shipping-notification noborder">
-              <tbody>
-                <tr>
-                  <th>
-                    <i
-                      className="fa fa-exclamation-triangle"
-                      aria-hidden="true"
-                    ></i>
-                    This order has
-                  </th>
-                </tr>
-                <tr>
-                  <td className="fx-coupon-des">
-                    <i className="pi pi-angle-double-right"></i>
-                    coupon/s <strong>{bookingData?.coupon}</strong> with total
-                    discount of{" "}
-                    <span className="postive_price_module_discount">
-                      {decodeHtml(bookingData?.coupon_discount)}
-                    </span>{" "}
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+              <table className="fx-billing-shipping-notification noborder">
+                <tbody>
+                  <tr>
+                    <th>
+                      <i
+                        className="fa fa-exclamation-triangle"
+                        aria-hidden="true"
+                      ></i>
+                      This order has
+                    </th>
+                  </tr>
+                  <tr>
+                    <td className="fx-coupon-des">
+                      <i className="pi pi-angle-double-right"></i>
+                      coupon/s <strong>{bookingData?.coupon}</strong> with total
+                      discount of{" "}
+                      <span className="postive_price_module_discount">
+                        {decodeHtml(bookingData?.coupon_discount)}
+                      </span>{" "}
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
 
-            {/* <table className="discountbox">
+              {/* <table className="discountbox">
               <tbody>
                 <tr>
                   <td>
@@ -295,9 +301,10 @@ export default function Thankyou() {
                 </tr>
               </tbody>
             </table> */}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
