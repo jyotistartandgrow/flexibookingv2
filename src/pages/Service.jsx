b8b21585738c6fbbe7253ce8818c6b793c7e32b7
jyotistartandgrow <jyotiprava@startandgrow.in>
2026-04-01 18:53:21 +0530

    class adedd

--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -949,6 +949,7 @@ export default function Service(props) {
           // style={{ width: "50vw" }}
           // breakpoints={{ "960px": "75vw", "641px": "100vw" }}
           className="fx-booking fx-main-modal-box"
+          contentClassName="fx-expand-details-popup"
         >
           <div
             className="fx-overlay"
