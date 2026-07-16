80250d67a6fd5ea93df3da016d7af74e802a7111
jyotistartandgrow <jyotiprava@startandgrow.in>
2026-07-16 18:15:57 +0530

    commit message

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -797,7 +797,6 @@ export default function Checkout(props) {
           </div>
         )}
         <div className="fx-inputgroup fx-checkboxcontainer">
-          <input type="checkbox" id="checkbox-checked" defaultChecked />
           <input
             type="checkbox"
             id="terms-checkbox"
